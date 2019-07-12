da3b4adb3089be5fb6ba9cef48d7deb9b7169b69
salazenas <salazenas@gmail.com>
2019-07-12 13:06:46 -0300

    WEB-101

--- src/js/events.js
+++ src/js/events.js
@@ -4,13 +4,23 @@ import { addTodo, toggleTodoState } from './actions';
 
 export function registerEventHandlers() {
     listen('click', '#addTodo', event => {
-        const todoInput = document.getElementById('todoInput');
-        todos.dispatch(addTodo(todoInput.value));
-        event.stopPropagation();
+        dispatchTodo()
     });
 
     listen('click', '.js_toggle_todo', event => {
         const id = Number.parseInt(event.target.getAttribute('data-id'), 10);
         todos.dispatch(toggleTodoState(id));
     });
+
+    listen('keyup', '#todoInput', event => {
+        if (event.key === 'Enter') {
+            dispatchTodo()
+        }
+    });
+}
+
+function dispatchTodo() {
+    const todoInput = document.getElementById('todoInput');
+    todos.dispatch(addTodo(todoInput.value));
+    event.stopPropagation();
 }
